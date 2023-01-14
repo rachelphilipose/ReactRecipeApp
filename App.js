7a0a104ff292117d50ca8e54f64934e9d63ca148
“rachelphilipose” <rachel@voltaric.com>
2023-01-14 15:24:35 -0500

    added nav stack to expo

--- App.js
+++ App.js
@@ -1,31 +1,66 @@
 import 'react-native-gesture-handler';
-//import { createStackNavigator } from '@react-navigation/stack';
-//import { NavigationContainer } from '@react-navigation/native'; 
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native'; 
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, TextInput, Pressable} from 'react-native';
 import React, {useState} from 'react';
 import HomePage from './screens/Home/home';
+//import { ApplicationProvider, theme } from 'bulma-native';
 
 
+const Stack = createStackNavigator();
 
-
-export default function App() {
+function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <Text style={styles.titleText}> Welcome to "Project" </Text>
     <Text style={styles.baseText}> Made for and by hungry college students, 
     this app will solve your cooking problems by helping you choose what to cook! </Text>
 
-    <Pressable style= {styles.button}>
+    <Pressable style= {styles.button} 
+    onPress={() => navigation.navigate('InputOutput')} >
         <Text style= {styles.innerText}> Let's Get Started</Text>
     </Pressable>
+
+
+
       <StatusBar style="auto"> </StatusBar>
      </View>
 
-   // <HomePage/>
+  );
+}
+
+function InputOutput() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titleText}> Input and Finished Recipie will go here!! </Text>
+      <StatusBar style="auto"> </StatusBar>
+     </View>
 
   );
 }
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="InputOutput" component={InputOutput} />
+
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
+
+
+
+
 
 const styles = StyleSheet.create({
   button: {
